ef8caba86f60b131596781b894a9198a5281d106
otarampinelli <otaviorampinellielias@gmail.com>
2022-06-02 00:22:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const names = require('./names.json')
+
+const { firstnames, lastnames } = names.data
+
+function nameGenerator(totalOfNames) {
+    const listOfGeneratedNames = []
+    let initializer = 0
+    while (initializer <= totalOfNames) {
+        let firstname = firstnames[Number((Math.random() * 9).toFixed())]
+        let lastname = lastnames[Number((Math.random() * 9).toFixed())]
+        listOfGeneratedNames.push(`${firstname} ${lastname}`)
+        initializer++
+    }
+
+    return listOfGeneratedNames
+}
+
+console.log(nameGenerator(10))
